e43aa385d30984418f4bb10953252e618883d6d7
Oleg Ershov <oecorango@gmail.com>
2023-06-15 20:17:24 +0400

    feat: add eslinter and prettier for task

--- css-selectors/src/index.ts
+++ css-selectors/src/index.ts
@@ -1,11 +1,1 @@
-import './style.scss'
-import './module/main'
-
-const dafa = document.querySelector('body');
-function pop(asdf: any):any {
-  console.log('12')
-  return asdf
-};
-
-console.log('12');
-let mmm = 12;
+import './style.scss';
