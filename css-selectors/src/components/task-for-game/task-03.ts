7a2d16aff478de9b611fda26b9f5e6c6b9954b2c
Oleg Ershov <oecorango@gmail.com>
2023-07-01 21:26:59 +0400

    feat: add task for 1-5 levels, change assets image

--- css-selectors/src/components/task-for-game/task-03.ts
+++ css-selectors/src/components/task-for-game/task-03.ts
@@ -1,11 +1,13 @@
 import { CreateEltment } from '../classes/Create-element';
 
 export function createLevel03(): void {
-  const elem1 = new CreateEltment('.current-task', 'div', 'paper');
-  const elem2 = new CreateEltment('.current-task', 'div', 'pillow');
-  const elem3 = new CreateEltment('.current-task', 'div', 't-shirt');
+  const elem1 = new CreateEltment('.current-task', 'div', 'car_1');
+  const elem2 = new CreateEltment('.current-task', 'div', 'car_2');
+  const elem3 = new CreateEltment('.current-task', 'div', 'car_3');
+  const elem4 = new CreateEltment('.current-task', 'div', 'train');
 
   elem1.create();
   elem2.create();
   elem3.create();
+  elem4.create();
 }
--- css-selectors/src/components/task-for-game/task-04.ts
+++ css-selectors/src/components/task-for-game/task-04.ts
@@ -1,13 +1,17 @@
 import { CreateEltment } from '../classes/Create-element';
 
-export function createLevel02(): void {
-  const elem1 = new CreateEltment('.current-task', 'div', 'car_3');
+export function createLevel04(): void {
+  const elem1 = new CreateEltment('.current-task', 'div', 'car_1');
   const elem2 = new CreateEltment('.current-task', 'div', 'car_2');
   const elem3 = new CreateEltment('.current-task', 'div', 'car_3');
   const elem4 = new CreateEltment('.current-task', 'div', 'train');
+  const nestedElem1 = new CreateEltment('.car_2', 'div', 'goofy');
+  const nestedElem2 = new CreateEltment('.car_3', 'div', 'goofy');
 
   elem1.create();
   elem2.create();
   elem3.create();
   elem4.create();
+  nestedElem1.create();
+  nestedElem2.create();
 }
