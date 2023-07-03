67bff6ac2d087fb9ed2b5ef8003f8387f8834511
Oleg Ershov <oecorango@gmail.com>
2023-07-03 11:13:24 +0400

    feat: update (change) task for level 6-7

--- css-selectors/src/components/game/task-forgame/task-06.ts
+++ css-selectors/src/components/game/task-forgame/task-06.ts
@@ -1,11 +1,17 @@
 import { CreateEltment } from '../../classes/Create-element';
 
 export function createLevel06(): void {
-  const elem1 = new CreateEltment('.current-task', 'div', 'car_3');
-  const elem2 = new CreateEltment('.current-task', 'div', 'car_3');
-  const elem3 = new CreateEltment('.current-task', 'div', 'train');
+  const elem1 = new CreateEltment('.current-task', 'div', 'car_horse');
+  const elem2 = new CreateEltment('.current-task', 'div', 'car_2');
+  const elem3 = new CreateEltment('.current-task', 'div', 'car_3');
+  const elem4 = new CreateEltment('.current-task', 'div', 'train');
+  const nestedElem1 = new CreateEltment('.car_2', 'div', 'small-horse', 'strobe');
+  const nestedElem2 = new CreateEltment('.car_3', 'div', 'small-horse', 'strobe');
 
   elem1.create();
   elem2.create();
   elem3.create();
+  elem4.create();
+  nestedElem1.create();
+  nestedElem2.create();
 }
--- css-selectors/src/components/game/task-forgame/task-07.ts
+++ css-selectors/src/components/game/task-forgame/task-07.ts
@@ -1,11 +1,19 @@
 import { CreateEltment } from '../../classes/Create-element';
 
 export function createLevel07(): void {
-  const elem1 = new CreateEltment('.current-task', 'div', 'car_3');
-  const elem2 = new CreateEltment('.current-task', 'div', 'car_3');
-  const elem3 = new CreateEltment('.current-task', 'div', 'train');
+  const elem1 = new CreateEltment('.current-task', 'div', 'null');
+  const elem2 = new CreateEltment('.current-task', 'div', 'car_2');
+  const elem3 = new CreateEltment('.current-task', 'div', 'car_3');
+  const elem4 = new CreateEltment('.current-task', 'div', 'train');
+  const nestedElem1 = new CreateEltment('.null', 'div', 'small-mas', 'strobe');
+  const nestedElem2 = new CreateEltment('.car_2', 'div', 'mice');
+  const nestedElem3 = new CreateEltment('.car_3', 'div', 'small-mice', 'strobe');
 
   elem1.create();
   elem2.create();
   elem3.create();
+  elem4.create();
+  nestedElem1.create();
+  nestedElem2.create();
+  nestedElem3.create();
 }
